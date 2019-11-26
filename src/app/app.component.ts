58a6a58e7f6b88b2a2fe4ba32a10deeffa157ac7
Rahul Kumar <rahulkumar151995@gmail.com>
2019-11-26 14:11:37 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdfViewer';
+  dataLocalUrl: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SafeUrlPipe } from './safe-url.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafeUrlPipe
   ],
   imports: [
     BrowserModule
